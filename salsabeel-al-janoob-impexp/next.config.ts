a90387de22f883196ce523b44822169eb937f1e8
Anandhuvimalan <anandhuvimalan1@gmail.com>
2025-03-02 16:32:22 +0530

    first1

--- salsabeel-al-janoob-impexp/next.config.ts
+++ salsabeel-al-janoob-impexp/next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Matches all routes
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Change to your domain if needed
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS, PUT, DELETE",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
